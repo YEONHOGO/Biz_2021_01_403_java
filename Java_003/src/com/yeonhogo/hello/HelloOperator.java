dede809f000473c95dee29bd4074f9ca976edbcc
YEONHOGO <jayser0768@gmail.com>
2021-02-02 14:11:58 +0900

    명령어

---
+++ Java_003/src/com/yeonhogo/hello/HelloOperator.java
@@ -0,0 +1,26 @@
+package com.yeonhogo.hello;
+
+public class HelloOperator {
+
+	/*
+	 * 클래스 이름 짓기  
+	 * 1. 명명 규칙
+	 * 첫글자는 영문대문자
+	 * 이후에는 영문 대소문자, 숫자, UnderScore(_)
+	 * 2. 명명규칙
+	 * 	Camel case로 이름짓기
+	 * 	가급적 2단어 이상으로 조합하여 짓자
+	 * 	각 단어가 시작될때는 대문자로 시작
+	 * 	숫자, UnderScore는 가급적 사용을 자제하자
+	 * 	클래스이름만 보고 하는일을 유추하도록 하자
+	 */
+	public static void main(String args[]){
+		
+		System.out.println(30 + 40);
+		System.out.println(30 * 40);
+		System.out.println(30 - 40);
+		System.out.println(30 / 40);
+		System.out.println(40 % 30);
+		
+	}
+}
---
+++ Java_003/src/com/yeonhogo/hello/Hello_01.java
@@ -0,0 +1,20 @@
+package com.yeonhogo.hello;
+
+public class Hello_01 {
+
+	public static void main(String args[]) {
+		
+		System.out.println("여기는 Hello_01 클래스");
+		System.out.println("여기는 Hello_O1 클래스");
+		System.out.println()
+		;
+		System.out.print("30 + 40 = ");
+		System.out.println(30 + 40);
+		
+		System.out.print("Republic ");
+		System.out.print("Of ");
+		System.out.println("Korea");
+		
+						
+	}
+}
