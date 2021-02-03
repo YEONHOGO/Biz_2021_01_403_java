4ee10ac8e87e41dacef768063628f69366b2b830
YEONHOGO <jayser0768@gmail.com>
2021-02-03 10:30:33 +0900

    집에서 연습

---
+++ Java_004/src/com/YEONHOGO/hello/HelloHomeWork.java
@@ -0,0 +1,13 @@
+package com.YEONHOGO.hello;
+
+public class HelloHomeWork {
+
+	/*
+	 *  이 클래스는 집에서 공부를 위해 추가한 것이다.
+	 */
+	public static void main(String args[]) {
+		
+		System.out.println("반갑습니다");
+		
+	}
+}
