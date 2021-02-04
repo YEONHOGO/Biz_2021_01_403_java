473525b2133a4181cfe3e495772f0ffbccc7d68b
YEONHOGO <jayser0768@gmail.com>
2021-02-04 16:33:34 +0900

    반복문

---
+++ Java_006/src/com/YEONHOGO/varriable/Loop_05.java
@@ -0,0 +1,33 @@
+package com.YEONHOGO.varriable;
+
+public class Loop_05 {
+
+	
+	public static void main(String[] args) {
+		int i = 1;
+		int intDan = 7;
+		for(int num = 1; num < 10; num += 1) {
+			
+			
+		}
+		
+		for(int num = 1; num < 100; num += 1) {
+			
+			
+		}
+		for(int num = 20; num < 42; num += 1) {
+			
+			System.out.println("안녕");
+			System.out.println(i);
+			i++;
+		}
+		// 시작하는값	비교하는값	증가하는값
+		for(int num = 12; num <= 20; num += 1) {
+			
+			System.out.println("반가워");
+		}
+		for(int num = 12; num <= 20; num +=2) {
+			System.out.println(num);	
+		}
+	}
+}
